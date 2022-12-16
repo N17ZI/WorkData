6d11a7ecabed4f912cbecc175a0caf3f6e467159
Niyaz <minecart881@gmail.com>
2022-12-16 18:59:31 +0300

    Добавьте файлы проекта.

---
+++ WorkData/AddEditPageT.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WorkData
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPageT.xaml
+    /// </summary>
+    public partial class AddEditPageT : Page
+    {
+        private tour_schedule _currentTour = new tour_schedule();
+        public AddEditPageT(tour_schedule selectedTour)
+        {
+            InitializeComponent();
+            
+            if (selectedTour != null)
+                _currentTour = selectedTour;
+            DataContext = _currentTour;
+            //ComboPres.ItemsSource = Database_circusEntities.GetContext().presentation.ToList();
+        }
+
+        private void BSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrEmpty(Convert.ToString(_currentTour.data_start)))
+                errors.AppendLine("Заполните ячейку");
+            if (string.IsNullOrEmpty(Convert.ToString(_currentTour.data_end)))
+                errors.AppendLine("Заполните ячейку");
+            if (string.IsNullOrEmpty(Convert.ToString(_currentTour.Place_spot)))
+                errors.AppendLine("Заполните ячейку");
+            if (_currentTour.presentation == null)
+                errors.AppendLine("Заполните ячейку");
+
+            if(errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if(_currentTour.Id_schedule == 0)
+                Database_circusEntities.GetContext().tour_schedule.Add(_currentTour);
+            try
+            {
+                Database_circusEntities.GetContext().SaveChanges();
+                MessageBox.Show("Saved");
+                NavigationService.Navigate(new TableTour());
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
---
+++ WorkData/TableTour.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WorkData
+{
+    /// <summary>
+    /// Логика взаимодействия для TableTour.xaml
+    /// </summary>
+    public partial class TableTour : Page
+    {
+        public TableTour()
+        {
+            InitializeComponent();
+            DGridTour.ItemsSource = Database_circusEntities.GetContext().tour_schedule.ToList();
+        }
+
+        private void BAdd_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddEditPageT(null));
+        }
+
+        private void BRemove_Click(object sender, RoutedEventArgs e)
+        {
+            var tourRemove = DGridTour.SelectedItems.Cast<tour_schedule>().ToList();
+            if(MessageBox.Show($"Are really want delete {tourRemove.Count}?","Warn",
+                MessageBoxButton.YesNo,MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    Database_circusEntities.GetContext().tour_schedule.RemoveRange(tourRemove);
+                    Database_circusEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Removed");
+                    DGridTour.ItemsSource = Database_circusEntities.GetContext().tour_schedule.ToList(); 
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+        private void BEdit_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddEditPageT((sender as Button).DataContext as tour_schedule));
+        }
+    }
+}
